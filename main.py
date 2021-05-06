d4c4b2735a1977a5e70b15de3ad54f5b168205ae
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-05-06 16:12:59 -0700

    Opened a connection to the database and read from it.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/bin/python
 
+from argparse import ArgumentParser
+from sqlite3 import connect
 
-def main():
-    print("Hello world")
+
+def main(database_file):
+    cur = connect(database_file).cursor()
+    tables = cur.execute("select name from sqlite_master where type = 'table'")
+
+    for table in tables:
+        print(table)
+        for row in cur.execute(f"select * from {table[0]}"):
+            print(row)
+
+    cur.connection.commit()
+    cur.close()
 
 
 if __name__ == "__main__":
-    main()
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "database_file", help="The file from which the database is loaded"
+    )
+    args = arg_parser.parse_args()
+    main(args.database_file)
